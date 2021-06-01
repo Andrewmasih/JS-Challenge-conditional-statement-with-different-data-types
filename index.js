1a50baf5507ad793df7f091a7f26dd362edb279c
Andrew Masih <armasih@outlook.com>
2021-06-01 09:21:33 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let adminEnabled = "false";
+
+if (adminEnabled === "false") {
+ console.log("you need to enable admin access to enter this part of the site"); }
+else {
+  console.log("access granted");
+} 
